6db03cff2b13d3543428026614fb3490ae166a20
Gustavo Viana Pedro <gvpedro@stefanini.com>
2026-07-02 17:24:14 -0300

    first commit

---
+++ src/main/java/br/com/Lutas/CadastroDeLutadores/CadastroDeLutadoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.Lutas.CadastroDeLutadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeLutadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeLutadoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/Lutas/CadastroDeLutadores/CadastroDeLutadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Lutas.CadastroDeLutadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeLutadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
