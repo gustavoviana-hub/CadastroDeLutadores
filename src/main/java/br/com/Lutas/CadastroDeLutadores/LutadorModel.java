830ebbb9eea871c6611dde5217cbc980ca75f9ec
Gustavo Viana Pedro <gvpedro@stefanini.com>
2026-07-03 16:05:36 -0300

    feat: Cria uma entidade apartir do LutadorModel

---
+++ src/main/java/br/com/Lutas/CadastroDeLutadores/LutadorModel.java
@@ -0,0 +1,102 @@
+package br.com.Lutas.CadastroDeLutadores;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class LutadorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String categoria;
+    private String nacionalidade;
+    private String estiloDeLuta;
+
+    private int idade;
+    private double peso;
+    private double altura;
+
+    // Com contrutores podemos contruir o lutador com as informações de uma vez.
+
+    public LutadorModel() {
+
+    }
+
+    public LutadorModel(String nome, String email, String categoria, String nacionalidade, String estiloDeLuta, int idade, double peso, double altura) {
+        this.nome = nome;
+        this.email = email;
+        this.categoria = categoria;
+        this.nacionalidade = nacionalidade;
+        this.estiloDeLuta = estiloDeLuta;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getEstiloDeLuta() {
+        return estiloDeLuta;
+    }
+
+    public void setEstiloDeLuta(String estiloDeLuta) {
+        this.estiloDeLuta = estiloDeLuta;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
