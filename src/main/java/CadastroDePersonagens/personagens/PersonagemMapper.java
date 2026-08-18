6abaff8e600cc1d4c3ae2d17880afa696a975c6e
Gustavo Viana Pedro <gvpedro@stefanini.com>
2026-08-18 11:20:02 -0300

    chore: Inclui a versao do DTO pela entidade e mapeia manualmente no PersonagemMapper os caminhos entidades xDTO

--- src/main/java/CadastroDePersonagens/personagens/PersonagemMapper.java
+++ src/main/java/CadastroDePersonagens/personagens/PersonagemMapper.java
@@ -1,4 +1,33 @@
 package CadastroDePersonagens.personagens;
 
+import CadastroDePersonagens.personagens.model.PersonagemModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PersonagemMapper {
+
+    public PersonagemModel map(PersonagemDTO personagemDTO){
+        PersonagemModel personagemModel = new PersonagemModel();
+        personagemModel.setId(personagemDTO.getId());
+        personagemModel.setNome(personagemDTO.getNome());
+        personagemModel.setEmail(personagemDTO.getEmail());
+        personagemModel.setImgUrl(personagemDTO.getImgUrl());
+        personagemModel.setRank(personagemDTO.getRank());
+        personagemModel.setMissoes(personagemDTO.getMissoes());
+
+        return personagemModel;
+    }
+
+    public PersonagemDTO map(PersonagemModel personagemModel){
+        PersonagemDTO personagemDTO = new PersonagemDTO();
+        personagemDTO.setId(personagemModel.getId());
+        personagemDTO.setNome(personagemModel.getNome());
+        personagemDTO.setEmail(personagemModel.getEmail());
+        personagemDTO.setImgUrl(personagemModel.getImgUrl());
+        personagemDTO.setRank(personagemModel.getRank());
+        personagemDTO.setMissoes(personagemModel.getMissoes());
+
+        return personagemDTO;
+    }
+
 }
