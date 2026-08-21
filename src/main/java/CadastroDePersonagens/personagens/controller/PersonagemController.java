9a0db828da7774a8e4624abd1e021619f3792733
Gustavo Viana Pedro <gvpedro@stefanini.com>
2026-08-21 15:41:34 -0300

    doc: Adiciona swagger na minha aplicação para documnetacao nas minhas rotas controller de personagens

--- src/main/java/CadastroDePersonagens/personagens/controller/PersonagemController.java
+++ src/main/java/CadastroDePersonagens/personagens/controller/PersonagemController.java
@@ -2,6 +2,10 @@ package CadastroDePersonagens.personagens.controller;
 
 import CadastroDePersonagens.personagens.PersonagemDTO;
 import CadastroDePersonagens.personagens.service.PersonagemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +24,11 @@ public class PersonagemController {
 
     // Adicinar personagem (Create)
     @PostMapping("/criar")
+    @Operation(summary = "Cria um novo personagem", description = "Rota cria um novo personagem e inclui no banco de dados")
+    @ApiResponses(value ={
+            @ApiResponse(responseCode = "201", description = "Personagem criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na criação do personagem")
+    })
     public ResponseEntity<String> criarNinja(@RequestBody PersonagemDTO persoangem){
         PersonagemDTO novoPersonagem = personagemService.criarPersoangem(persoangem);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -28,6 +37,11 @@ public class PersonagemController {
 
     // Mostrar personagem por Id (Create)
     @GetMapping("/listar/{id}")
+    @Operation(summary = "Lista o personagem por id", description = "Rota list um perosnagem pelo seu id")
+    @ApiResponses(value ={
+            @ApiResponse(responseCode = "200", description = "Personagem encontrado"),
+            @ApiResponse(responseCode = "404", description = "Perosnagem não encontrado")
+    })
     public ResponseEntity<?> listarTodosOsPersonagens(@PathVariable Long id){
         PersonagemDTO personagem = personagemService.listarPersonagemPorId(id);
         if (personagem !=null){
@@ -40,6 +54,7 @@ public class PersonagemController {
 
     // Mostrar todos os personagens (Read)
     @GetMapping("/listar")
+    @Operation(summary = "Listar todos os personagens", description = "Essa rota mostra todos os personagens que tem no meu banco de dados")
     public ResponseEntity<List<PersonagemDTO>> listarPersonagensPorId(){
         List<PersonagemDTO> personagem = personagemService.listarPesronagens();
         return ResponseEntity.ok(personagem);
@@ -47,7 +62,17 @@ public class PersonagemController {
 
     // Atualizar Personagem
     @PutMapping("/alterar/{id}")
-    public ResponseEntity<?> alterarPersonagemPorId(@PathVariable Long id, @RequestBody PersonagemDTO personagemAtualizado){
+    @Operation(summary = "Altera o personagem por id", description = "Rota altera personagem pelo seu id")
+    @ApiResponses(value ={
+            @ApiResponse(responseCode = "201", description = "Personagem alterado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Personagem não encontrado, não foi possivel alterar")
+    })
+    public ResponseEntity<?> alterarPersonagemPorId(
+            @Parameter(description = "Usuario manda o id no caminho da requisição")
+            @PathVariable Long id,
+            @Parameter(description = "Usuario manda os dados do personagem a ser atualizado no corpo da requisição")
+            @RequestBody PersonagemDTO personagemAtualizado){
+
         PersonagemDTO personagem = personagemService.atualizarPersonagem(id, personagemAtualizado);
 
         if (personagem != null){
